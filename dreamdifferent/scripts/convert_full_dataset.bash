b6453ffcca7ac0a62bb29840230463232c9bf8eb
ErykHalicki <erykhalicki0@gmail.com>
2026-03-19 22:00:42 +0100

    real conversion scripts

--- dreamdifferent/scripts/convert_full_dataset.bash
+++ dreamdifferent/scripts/convert_full_dataset.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#SBATCH --time=10:00
-#SBATCH --output=convert_dataset_test.out
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=2048MB
+#SBATCH --output=convert_dataset.out
 
 module load eth_proxy #need to load this to access anything outside the ETH network on a cluster node
 cd ~
@@ -11,4 +13,3 @@ python dreamdifferent/scripts/egoverse_to_lerobotv2.py \
     --src_dir /cluster/work/cvg/data/Egoverse/raw_timesynced_h5/bag_groceries/ \
     --tgt_path /cluster/scratch/ehalicki \
     --task_type bag_groceries \
-    --debug
