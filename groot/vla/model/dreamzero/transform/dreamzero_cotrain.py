3a4a6da1d04cdfdb5b69934f2ad757f6407977f8
DHKim0428 <sidsince0428@gmail.com>
2026-05-07 19:13:53 +0000

    add so101

--- groot/vla/model/dreamzero/transform/dreamzero_cotrain.py
+++ groot/vla/model/dreamzero/transform/dreamzero_cotrain.py
@@ -126,6 +126,8 @@ def collate(features: List[dict], tokenizer: AutoTokenizer, num_views=3, embodim
                         processed_item = "A multi-view video shows that a robot " + processed_item.lower() + " The video is split into four views: The top-left view shows the top camera, the top-right view shows the right camera, the bottom-left view shows the left camera, and the bottom-right view is a black screen. The robot " + processed_item.lower()
                     elif elem["embodiment_id"] == embodiment_tag_mapping[EmbodimentTag.FRANKA.value]:
                         processed_item = "A multi-view video shows that a bimanual robot " + processed_item.lower() + " The video is split into two views: The left view shows the egocentric aria RGB camera, and the right view shows the front-facing OAK-D camera. The robot " + processed_item.lower()
+                    elif elem["embodiment_id"] == embodiment_tag_mapping[EmbodimentTag.SO101.value]:
+                        processed_item = "A multi-view video shows that a robot " + processed_item.lower() + " The video is split into two views: The left view shows the front camera, and the right view shows the wrist camera. The robot " + processed_item.lower()
                     else:
                         raise ValueError(f"Embodiment ID {elem['embodiment_id']} not supported.") 
                     output_values.append(processed_item)  
@@ -150,6 +152,8 @@ def collate(features: List[dict], tokenizer: AutoTokenizer, num_views=3, embodim
                         item = "A multi-view video shows that a robot " + str(item).lower() + " The video is split into four views: The top-left view shows the top camera, the top-right view shows the right camera, the bottom-left view shows the left camera, and the bottom-right view is a black screen. The robot " + str(item).lower()
                     elif elem["embodiment_id"] == embodiment_tag_mapping[EmbodimentTag.FRANKA.value]:
                         item = "A multi-view video shows that a bimanual robot " + str(item).lower() + " The video is split into two views: The left view shows the egocentric aria RGB camera, and the right view shows the front-facing OAK-D camera. The robot " + str(item).lower()
+                    elif elem["embodiment_id"] == embodiment_tag_mapping[EmbodimentTag.SO101.value]:
+                        item = "A multi-view video shows that a robot " + str(item).lower() + " The video is split into two views: The left view shows the front camera, and the right view shows the wrist camera. The robot " + str(item).lower()
                     else:
                         raise ValueError(f"Embodiment ID {elem['embodiment_id']} not supported.")   
                     output_values.append(item)
@@ -639,4 +643,3 @@ class DreamTransform(InvertibleModalityTransform):
 
     def __call__(self, data: dict) -> dict:
         return self.apply(data)
-
